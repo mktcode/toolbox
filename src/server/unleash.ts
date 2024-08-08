1409941171d8952789a14400c74348114e240aba
mktcode <kontakt@markus-kottlaender.de>
2024-08-08 22:12:02 +0200

    feat: add unleash

---
+++ src/server/unleash.ts
@@ -0,0 +1,14 @@
+import { evaluateFlags, flagsClient, getDefinitions } from "@unleash/nextjs";
+
+export const getFlags = async () => {
+  const definitions = await getDefinitions({
+    fetchOptions: {
+      next: { revalidate: 15 },
+    },
+  });
+
+  const { toggles } = evaluateFlags(definitions);
+  const flags = flagsClient(toggles);
+
+  return flags
+};
