7f4355c997837a41d5249646b44d63209fa84e6c
mktcode <kontakt@markus-kottlaender.de>
2024-08-09 19:40:54 +0200

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,11 +207,17 @@ export default async function ProjectsPage() {
 
       {flags.isEnabled("landingpage.philosphy") && <div className="bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
-          <h1 className="mb-6 text-5xl">
+          <h1 className="mb-12 text-5xl">
             Philosophy
           </h1>
           <div className="text-2xl text-gray-500 leading-normal">
-          This is no ordinary product. It&apos;s a solution designed primarily to serve the developers who built it, rather than just the end customer. After years in the software industry, we&apos;ve repeatedly encountered the same issues. Junior developers struggle to grow because management and product decisions are made by those with little understanding of software development. Senior developers are reduced to emergency bug fixers, while juniors remain stuck at the &quot;quick and dirty&quot; level.
+          This is no ordinary product. After years in the software industry, I have repeatedly encountered the same issues. Junior developers struggle to grow because management and product decisions are made by those with little understanding of software development. Senior developers are reduced to emergency bug fixers, while juniors remain stuck at the &quot;quick and dirty&quot; level.<br />
+          <br />
+          The primary purpose of this &quot;product&quot;, is to give young developers a chance to work at their own pace and learn software development properly, in a real world scenario. The customer is NOT always right here. Our devs are. They just happen to be customers too, so they can learn what &quot;The customer is always right.&quot; really means, operationally, from both perspectives. They learn to design software with the problem domain in mind, rather than putting the cart before the horse.<br />
+          <br />
+          Unless you are a junior developer yourself, this product is likely to appeal to you only if you happend to need what is currently there or are searching for skilled developers who prioritize accuracy at a consistent speed and are capable of fixing boken processes. This is the product here. Not necessarily what the header says. We are not the club that wins trophies. We are the club that other clubs buy players from.<br />
+          <br />
+          If you are somebody else, who is actually just interested in the features currently offered here, be assured that if you pay, you will get what you paid for. If you have something to complain about, you talk to me. If you have ideas and useful feedback, you talk to the devs, personally.
           </div>
         </div>
       </div>}
