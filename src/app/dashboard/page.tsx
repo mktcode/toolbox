cf3f52e5518232e3926290de0bc30a5e9c2c72fd
mktcode <kontakt@markus-kottlaender.de>
2024-08-15 22:54:13 +0200

    wip

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,7 +40,7 @@ export default async function DashboardPage() {
 
   return (
     <>
-      <div className="min-h-full">
+      <div className="flex min-h-full flex-col">
         <Disclosure as="nav" className="bg-gray-800">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
@@ -206,9 +206,18 @@ export default async function DashboardPage() {
             </h1>
           </div>
         </header>
-        <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+        <main className="mx-auto flex w-full max-w-7xl grow flex-col px-4 py-6 sm:px-6 lg:px-8">
+          <div className="mt-auto rounded-md shadow-md transition-all">
+            <textarea
+              className="block w-full resize-none rounded-md rounded-b-none border-none p-6 text-lg focus:border-gray-400 focus:ring-gray-400"
+              rows={6}
+              placeholder="Chat with the AI ..."
+            />
+            <div className="flex rounded-b-md bg-gray-100 p-2">
+              <button className="ml-auto rounded-md bg-gray-800 px-4 py-2 text-white">
+                Send
+              </button>
+            </div>
           </div>
         </main>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`h-full bg-gray-100 ${GeistSans.variable}`}>
+    <html lang="en" className={`h-full bg-gray-200 ${GeistSans.variable}`}>
       <body className="h-full">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
