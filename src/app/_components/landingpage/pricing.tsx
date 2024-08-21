4a069d6eed7e17553662b0f85a2a86ca3f7b3393
mktcode <kontakt@markus-kottlaender.de>
2024-08-22 01:22:35 +0200

    refactor: Update landing page layout and pricing section

--- src/app/_components/landingpage/pricing.tsx
+++ src/app/_components/landingpage/pricing.tsx
@@ -1,10 +1,10 @@
 import { CheckIcon } from "@heroicons/react/20/solid";
 
 const includedFeatures = [
-  "Private forum access",
-  "Member resources",
-  "Entry to annual conference",
-  "Official member t-shirt",
+  "Chat with AI",
+  "Chat with me",
+  "Use AI Templates",
+  "Newsletter",
 ];
 
 export default function Pricing() {
@@ -24,12 +24,13 @@ export default function Pricing() {
         <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-              Lifetime membership
+              Garbage in, garbage out! Use at your own risk.
             </h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
-              amet indis perferendis blanditiis repellendus etur quidem
-              assumenda.
+              For the time being the tools will assume some basic knowledge of
+              prompt engineering. If you are unsatisfied with the results,
+              please let me know. Maybe I can help. There's no refund option,
+              yet.
             </p>
             <div className="mt-10 flex items-center gap-x-4">
               <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
@@ -52,7 +53,7 @@ export default function Pricing() {
               ))}
             </ul>
           </div>
-          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+          <div className="p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
                 <p className="text-base font-semibold text-gray-600">
@@ -60,21 +61,21 @@ export default function Pricing() {
                 </p>
                 <p className="mt-6 flex flex-col items-center justify-center gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    $1.00 USD
+                    $1.00
                   </span>
                   <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
                     per 1 Million tokens
                   </span>
+                  <p className="mt-3 text-xs leading-5 text-gray-600">
+                    (Could be cheaper than $20 per month.)
+                  </p>
                 </p>
                 <a
                   href="#"
                   className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get access
+                  Sign up and get $1 for free now!
                 </a>
-                <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Invoices and receipts available for easy company reimbursement
-                </p>
               </div>
             </div>
           </div>
