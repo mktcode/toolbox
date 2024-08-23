bbe927d2065868309efd8876e9fc23ae22d6e0c4
mktcode <kontakt@markus-kottlaender.de>
2024-08-23 23:48:35 +0200

    refactor: Remove unused AI feature from seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,45 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const WELCOME_AMOUNT = 1000;
-
 async function main() {
-  const admin = await prisma.user.create({
-    data: {
-      name: "mktcode",
-      email: "kontakt@markus-kottlaender.de",
-      currentBalance: WELCOME_AMOUNT,
-    },
-  });
-
-  await prisma.account.create({
-    data: {
-      provider: "github",
-      providerAccountId: "6792578",
-      userId: admin.id,
-      type: "oauth",
-    },
-  });
-
-  await prisma.topUp.create({
-    data: {
-      amount: WELCOME_AMOUNT,
-      note: "Welcome!",
-      confirmedAt: new Date(),
-      userId: admin.id,
-    },
-  });
-
-  await prisma.template.createMany({
-    data: [
-      {
-        name: "Native Speaker",
-        description: "Refine your text with the quality of a native speaker.",
-        body: "Hello, {{name}}!",
-        userId: admin.id,
-      },
-    ],
-  });
+  // Nothing to seed
 }
 
 main()
