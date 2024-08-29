6125b194a6cf624533c9bd409916a221e8c80608
mktcode <kontakt@markus-kottlaender.de>
2024-08-29 13:09:40 +0200

    refactor: Update newMessageInput component to remove unnecessary button and improve styling

--- src/app/template/[templateId]/[chatSessionId]/_components/newMessageInput.tsx
+++ src/app/template/[templateId]/[chatSessionId]/_components/newMessageInput.tsx
@@ -66,19 +66,13 @@ export default function NewMessageInput({
     <div className="fixed bottom-0 left-0 right-0 bg-gradient-to-t from-gray-200 to-transparent">
       <div className="mx-auto flex w-full max-w-7xl grow flex-col px-4 py-6 sm:px-6 lg:px-8">
         <div className="rounded-md shadow-md transition-all">
-          <div className="relative">
-            <textarea
-              className="block w-full resize-none rounded-md rounded-b-none border-none p-6 text-lg placeholder:text-gray-400 focus:border-gray-400 focus:ring-gray-400 focus:placeholder:text-gray-300"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              rows={4}
-              placeholder="Chat with the AI ..."
-            />
-            <Button className="button shy mini absolute right-1 top-1 border-none py-1 !font-normal opacity-50 hover:opacity-100">
-              <TableCellsIcon className="mr-2 h-4 w-4 opacity-40" />
-              Use Template
-            </Button>
-          </div>
+          <textarea
+            className="block w-full resize-none rounded-md rounded-b-none border-none p-6 text-lg placeholder:text-gray-400 focus:border-gray-400 focus:ring-gray-400 focus:placeholder:text-gray-300"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows={4}
+            placeholder="Chat with the AI ..."
+          />
           <div className="flex rounded-b-md bg-gray-100 p-2">
             <button className="rounded-md px-4 py-2 hover:bg-gray-200">
               <MicrophoneIcon className="h-6 w-6 text-gray-400" />
